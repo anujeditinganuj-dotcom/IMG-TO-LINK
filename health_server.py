5a320c3e2ecb06dfce99929ac1cbd8d0bd57a4ce
anujeditinganuj-dotcom <anujeditinganuj@gmail.com>
2026-06-12 23:30:22 +0530

    Update health_server.py

--- health_server.py
+++ health_server.py
@@ -23,7 +23,7 @@ def health():
     return jsonify({"status": "ok"}), 200
 
 def run_flask():
-    port = int(os.getenv("PORT", 8080))
+    port = int(os.getenv("PORT", 5000))
     app.run(host="0.0.0.0", port=port, debug=False)
 
 def start_health_server():
