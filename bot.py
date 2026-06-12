3ebcc17f080694c8dd64876ce993418ef522b5b1
anujeditinganuj-dotcom <anujeditinganuj@gmail.com>
2026-06-12 23:47:46 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -37,13 +37,13 @@ from aiogram.types import (
 )
 
 # ─────────────────────── config ────────────────────────────────
-BOT_TOKEN     = os.getenv("BOT_TOKEN", "YOUR_BOT_TOKEN_HERE")
+BOT_TOKEN     = os.getenv("BOT_TOKEN", "7512964694:AAFNZbJy6RBIuSUQtNLiQhiRTK1ccBczPeg")
 IMGBB_KEY     = os.getenv("IMGBB_KEY", "70ee073479a71bce3aed7598ace7eee8")
 IMGBB_URL     = "https://api.imgbb.com/1/upload"
 
 # 👇 Apna channel aur group link yahan dalo
-UPDATE_CHANNEL = os.getenv("UPDATE_CHANNEL", "https://t.me/your_channel")
-SUPPORT_GROUP  = os.getenv("SUPPORT_GROUP",  "https://t.me/your_group")
+UPDATE_CHANNEL = os.getenv("UPDATE_CHANNEL", "https://t.me/log_ak_bots")
+SUPPORT_GROUP  = os.getenv("SUPPORT_GROUP",  "https://t.me/log_ak_bots")
 
 EXPIRY_OPTIONS = {
     "⏰ 1 Hour":  3600,
