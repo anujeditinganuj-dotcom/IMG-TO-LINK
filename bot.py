d125efa8056dfc28cbb71e690b98ba3d98250885
anujeditinganuj-dotcom <anujeditinganuj@gmail.com>
2026-06-13 13:46:02 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -31,7 +31,7 @@ except ImportError:
     from pyrogram.enums import ParseMode
 
 # ─────────────────────── config ────────────────────────────────
-BOT_TOKEN      = os.getenv("BOT_TOKEN", "7512964694:AAFz7d7LoMLPm6z2P5wVKtJl_kKDN4JTDmo")
+BOT_TOKEN      = os.getenv("BOT_TOKEN", "7512964694:AAGnG9S83rpDtkqoE8EFzwAWcKw0jfLRzN4")
 API_ID         = int(os.getenv("API_ID", "37476811"))
 API_HASH       = os.getenv("API_HASH", "7aa60670b871050820086c6267371ee6")
 IMGBB_KEY      = os.getenv("IMGBB_KEY", "70ee073479a71bce3aed7598ace7eee8")
